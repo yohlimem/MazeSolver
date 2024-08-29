40e815112d0a8faadf1091c1bf929b7df0230d66
Yahli Mariansky <yahlimem@gmail.com>
2024-08-29 22:00:00 +0300

    added GUI

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 use std::{time::Duration, vec};
+use nannou::prelude::*;
+use nannou_egui::{self, egui, Egui};
 
 use nannou::prelude::*;
 mod Nodes;
-mod Astar;
-use crate::Astar::AStar;
 use crate::Nodes::Node;
 use std::collections::HashSet;
 
 // TODO: Make it so it doesnt trace over the same node twice.
 struct Model {
-    _window: window::Id,
+    egui: Egui,
     nodes: Vec<Vec<Node>>,
     maze_size: usize,
+    step_speed: f32,
     walker: (Vec2, Vec2, usize),
     explored_nodes: Vec<Vec2>,
     traced_nodes: HashSet<usize>,
@@ -22,12 +23,15 @@ fn main() {
 }
 
 fn model(app: &App) -> Model {
-    let _window = app.new_window().view(view).build().unwrap();
+
+    let window_id: WindowId = app.new_window().view(view).raw_event(raw_window_event).build().unwrap();
+    let window = app.window(window_id).unwrap();
+    let egui = Egui::from_window(&window);
 
     let mut nodes = Vec::new();
 
 
-    let maze_size = 20;
+    let maze_size = 10;
 
     for i in 0..maze_size {
         let mut row = Vec::new();
@@ -51,17 +55,36 @@ fn model(app: &App) -> Model {
     
 
     Model {
-        _window,
+        egui,
         nodes,
         maze_size,
+        step_speed: 60.0,
         walker: (vec2(0.0, 0.0), vec2(1.0, 0.0), 1),
         explored_nodes: vec![vec2(0.0, 0.0)],
         traced_nodes: HashSet::new(),
     }
 }
+fn raw_window_event(_app: &App, model: &mut Model, event: &nannou::winit::event::WindowEvent){
+    model.egui.handle_raw_event(event);
+}
+fn render_egui(egui: &mut Egui, murica: &mut f32) {
+    let egui = egui;
+    // egui.set_elapsed_time(update.since_start);
+
+    let ctx = egui.begin_frame();
+
+    egui::Window::new("Rum window").show(&ctx, |ui| {
+        ui.label("res"); // template
+        ui.add(egui::Slider::new(murica, 1.0..=40.0));
+    });
+}
 
 
 fn update(app: &App, model: &mut Model, _update: Update) {
+    render_egui(&mut model.egui, &mut model.step_speed);
+    if app.elapsed_frames() as f32 % model.step_speed != 0.0 {
+        return;
+    }
     let direction_list = [vec2(1.0,0.0), vec2(0.0,1.0), vec2(-1.0,0.0), vec2(0.0,-1.0)];
     let random_dir = random_range(0, 4);
     let random_dir = direction_list[random_dir];
@@ -121,6 +144,9 @@ fn update(app: &App, model: &mut Model, _update: Update) {
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
+    // if app.elapsed_frames() as f32 % model.step_speed != 0.0 {
+    //     return;
+    // }
     let draw = app.draw();
     draw.background().color(Rgb8::new(30, 203, 225));
 
@@ -135,11 +161,23 @@ fn view(app: &App, model: &Model, frame: Frame) {
     }
     model.nodes[0][0].draw(&draw, RED);
 
-    // model.walker
+    draw.ellipse()
+        .xy((model.walker.0 - (model.maze_size / 2) as f32) * Node::DIST)
+        .radius(20.0)
+        .color(GREEN);
+
+    draw.arrow()
+        .start((model.walker.0 - (model.maze_size / 2) as f32) * Node::DIST)
+        .end((model.walker.0 - (model.maze_size / 2) as f32) * Node::DIST + (model.walker.0 - model.walker.1) * 20.0)
+        .weight(2.0)
+        .color(BLACK);
+
 
     
 
     draw.to_frame(app, &frame).unwrap();
+    model.egui.draw_to_frame(&frame).unwrap();
+
 }
 
 fn is_outside(next_pos: Vec2, maze_size: usize) -> bool{
